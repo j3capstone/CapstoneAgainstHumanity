30256322edbaa2cf8faf3f5f3abc7e92d09f93bd
James Thomason, Jr <james@jamesthomasonjr.com>
2015-03-14 15:25:24 -0400

    Able to create a game now!

--- app/socket.js
+++ app/socket.js
@@ -1,15 +1,41 @@
 module.exports = function (app, server) {
     'use strict';
 
-    var io = require('socket.io')(server);
     var hashIDs = new require('hashids')('capstonesalt');
-    var idCount;
+    var idCount = 0;
 
-    var sessions;
+    var games;
 
     var models = {
+        Player: function (name) {
+            return {
+                name: name
+            }
+        },
+        Game: function (creator) {
+            return {
+                creator: creator,
+                createdOn: Date.now(),
+                currentRound: null
+            }
+        }
     };
 
+    var io = require('socket.io')(server);
     io.on('connection', function (socket) {
+        io.emit('gameList', games);
+
+        socket.on('join', function (username, gameId){
+        });
+
+        socket.on('createGame', function (creator) {
+            var gameId = hashIDs.encode(idCount);
+            idCount++;
+            games = games || {};
+            games[gameId] = new models.Game(creator);
+
+            io.emit('gameList', games);
+            socket.emit('gameCreated', gameId);
+        });
     });
 }
--- public/source/controllers/start.js
+++ public/source/controllers/start.js
@@ -1,5 +1,29 @@
 var app = angular.module('capstone');
 
-app.controller('StartController', function() {
+app.controller('StartController', function($log, $scope, $rootScope, $cookieStore, $location) {
     'use strict';
+
+    var socket = io();
+
+    $scope.user = {};
+
+    $scope.create = function(){
+        $rootScope.user = $scope.user;
+        $cookieStore.put('user', $scope.user);
+
+        socket.emit('createGame', $scope.user.username);
+        $log.log("createGame");
+    };
+
+    socket.on('gameCreated', function (id) {
+        $log.log("game created");
+        $log.log('/'+id);
+        $location.path('/'+id);
+        $scope.$apply();
+    });
+
+    socket.on('gameList', function (games) {
+        $scope.games = games;
+        $scope.$apply();
+    });
 });
