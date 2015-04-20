5f06fc11051ead7f620d1790eba1032abb0b6782
James Thomason <JamesThomason@QuickenLoans.com>
2015-04-20 17:24:57 -0400

    Switch comment style

--- app/utils/arrayTools.js
+++ app/utils/arrayTools.js
@@ -1,15 +1,15 @@
 module.exports = {
-    //A bunch of functions that I wrote to handle array weirdness.
-    //Felt like they would serve better inside their own utility object, so here they are!
+    /* A bunch of functions that I wrote to handle array weirdness. */
+    /* Felt like they would serve better inside their own utility object, so here they are! */
     ObjectToArray: function (object) {
         return Object.keys(object).map(function(key) { return object[key] });
     },
-    //I had the need to clone an array as to not modify the original
-    //Mainly to clone the master list of cards before shuffling and drawing cards for individual games
+    /* I had the need to clone an array as to not modify the original */
+    /* Mainly to clone the master list of cards before shuffling and drawing cards for individual games */
     Clone: function (array) {
         return array.splice(0);
     },
-    //Some fancy shuffle that I rewrote from Stack Overflow
+    /* Some fancy shuffle that I rewrote from Stack Overflow */
     Shuffle: function (array) {
         var counter = array.length;
         var index;
@@ -27,8 +27,8 @@ module.exports = {
 
         return array;
     },
-    //...This game has some inappropriate cards. This filters some of the worst ones out.
-    //...Since there are so many cards and so many are inappropriate, we opted to filter by acceptable choices instead of unacceptable.
+    /* ...This game has some inappropriate cards. This filters some of the worst ones out. */
+    /* ...Since there are so many cards and so many are inappropriate, we opted to filter by acceptable choices instead of unacceptable. */
     Filter: function (array) {
         /* Filter to a select few somewhat more class-appropriate cards */
         var allowed = [18, 26, 43, 47, 83, 95, 111, 143, 174, 190, 193,
